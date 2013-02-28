84193193df26ec02db14dabcad1db7e4eee6c47d
Cyrus Harrison <cyrush@llnl.gov>
2013-02-28 10:20:47 -0800

    added rebuild / reload util to make it easier to load changes to the pylulesh module into an ipython notebook

---
+++ notebooks/notebook_utils.py
@@ -0,0 +1,34 @@
+"""
+ file: notebook_utils.py
+ author: Cyrus Harrison <cyrush@llnl.gov>
+ created: 2/28/2013
+ description:
+    rebuild and reload the pylulesh module within an ipython notebook.
+
+"""
+
+import sys
+import os
+import subprocess
+import cStringIO
+
+import pylulesh
+
+from os.path import join as pjoin
+
+from IPython.lib.deepreload import reload as dreload
+
+
+def rebuild():
+    cwd = os.getcwd()
+    setup_path = os.path.abspath(pjoin(os.path.split(__file__)[0],".."))
+    os.chdir(setup_path)
+    subprocess.call(sys.executable  + " setup.py build",shell=True)
+    subprocess.call(sys.executable  + " setup.py install",shell=True)
+    os.chdir(cwd)
+    # mute std out for the dreload call
+    capture = cStringIO.StringIO()
+    orig_stdout = sys.stdout
+    sys.stdout = capture
+    dreload(pylulesh)
+    sys.stdout = orig_stdout
