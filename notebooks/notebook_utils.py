423fae3756601c19ac5c424873da20d3d46acf7d
Cyrus Harrison <cyrush@llnl.gov>
2013-02-28 11:55:40 -0800

    added support for importing visit into the ipython notebook

--- notebooks/notebook_utils.py
+++ notebooks/notebook_utils.py
@@ -32,3 +32,42 @@ def rebuild():
     sys.stdout = capture
     dreload(pylulesh)
     sys.stdout = orig_stdout
+
+def visit_bin_path():
+    for path in os.environ["PATH"].split(":"):
+        v_bin =pjoin(path,"visit").strip()
+        if os.path.isfile(v_bin):
+            return v_bin
+
+def visit_module_path(ver):
+    v_bin = visit_bin_path()
+    if v_bin is None:
+        return None
+    ver_root = os.path.abspath(pjoin(os.path.split(v_bin)[0],"..",ver))
+    arch =  None
+    for test_arch  in ["linux-x86_64","darwin-x86_64"]:
+        if os.path.isdir(pjoin(ver_root,test_arch)):
+            arch = test_arch
+    if arch is None:
+        return None
+    arch_root = pjoin(ver_root,arch)
+    py_root   = pjoin(arch_root,"lib","site-packages")
+    return py_root
+
+def load_visit_module(ver):
+    mod_path = visit_module_path(ver)
+    if mod_path is None:
+        print "[Error: Could not find VisIt %s - make `visit' is in your path.]" % ver
+        return
+    sys.path.insert(0,mod_path)
+    import visit
+    visit.AddArgument("-nowin")
+    visit.AddArgument("-v %s" % ver)
+    # squash non-sense in VisIt 2.5.2
+    capture = cStringIO.StringIO()
+    orig_stdout = sys.stdout
+    sys.stdout = capture
+    visit.Launch()
+    sys.stdout = orig_stdout
+
+
