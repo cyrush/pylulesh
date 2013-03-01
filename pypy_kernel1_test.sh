66a45fb9cb0bfec0994e37d39ae66da312faefb6
Gregory L. Lee <lee218@llnl.gov>
2013-03-01 11:43:33 -0800

    moved pypy test

--- pypy_kernel1_test.sh
+++ pypy_kernel1_test.sh
@@ -5,20 +5,20 @@ export PYTHONPATH=$PWD/src:$PYTHONPATH
 
 function build
 {
-    if [[ -e tests/target_test_kernel1-c ]]
+    if [[ -e pypy-tests/target_test_kernel1-c ]]
     then
         return 0
     fi
-    python ./libs/pypy-pypy/pypy//translator/goal/translate.py tests/target_test_kernel1.py
-    mv target_test_kernel1-c tests/
+    python ./libs/pypy-pypy/pypy//translator/goal/translate.py pypy-tests/target_test_kernel1.py
+    mv target_test_kernel1-c pypy-tests/
 }
 
 function runtest
 {
     for dim in 5 25 50
     do
-        time python tests/target_test_kernel1.py $dim
-        time ./tests/target_test_kernel1-c $dim
+        time python pypy-tests/target_test_kernel1.py $dim
+        time ./pypy-tests/target_test_kernel1-c $dim
     done
 }
 
