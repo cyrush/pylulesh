7501767bf9d09f9e6ee3839bee012d9d2dec7e33
Gregory L. Lee <lee218@llnl.gov>
2013-03-01 12:42:09 -0800

    added kernel 2 test

--- pypy_kernel2_test.py
+++ pypy_kernel2_test.py
@@ -6,24 +6,24 @@ import json
 python_setup = """
 import os
 def run_python(dim):
-    os.system("env PYTHONPATH=$PWD/src python ./pypy-tests/target_test_kernel1.py %s" %dim)
+    os.system("env PYTHONPATH=$PWD/src python ./pypy-tests/target_test_kernel2.py %s" %dim)
 """
 
 pypy_setup = """
 import os
 def run_pypy(dim):
-    os.system("./pypy-tests/target_test_kernel1-c %s" %dim)
+    os.system("./pypy-tests/target_test_kernel2-c %s" %dim)
 """
 
 results = {}
-testname = 'python_kernel1'
+testname = 'python'
 res = {}
 for dim in sys.argv[1:]:
     t = timeit.Timer("run_python(%s)"%dim, python_setup)
     res[dim] = t.timeit(5)/5.0
 results[testname] = res
 
-testname = 'pypy_kernel1'
+testname = 'pypy'
 res = {}
 for dim in sys.argv[1:]:
     t = timeit.Timer("run_pypy(%s)"%dim, pypy_setup)
@@ -31,4 +31,4 @@ for dim in sys.argv[1:]:
 results[testname] = res
 
 print results
-json.dump(results, open("pypy_kernel1_timing_results.json", "w"))
+json.dump(results, open("pypy_kernel2_timing_results.json", "w"))
